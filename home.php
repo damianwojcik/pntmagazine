d3d5b35d35288a1c0817633f05c9705b8176a390
Damian Wójcik <khamian@gmail.com>
2016-07-22 13:35:45 +0200

    Added banners functionality (acf repeater field).

--- home.php
+++ home.php
@@ -10,20 +10,13 @@
 
 		<div class="row">
 
-			<div class="span12">
+			<div class="span6">
 
 				<p class="intro">
+
 					<?php the_field('form-intro', 'option'); ?>
+
 				</p>
-
-			</div>
-
-		</div>
-		<!-- END row -->
-
-		<div class="row">
-
-			<div class="span12 half">
 
 
 				<!-- Ninja form -->
@@ -35,9 +28,57 @@
 				
 
 			</div>
-			<!-- END span12 -->
+			<!-- END span6-->
 
-			<div class="img-bear"></div>
+			<div class="span6">
+
+				<?php
+
+				if( have_rows('banners', 'option') ): ?>
+
+					<ul class="list-banners">
+
+
+					<?php while ( have_rows('banners', 'option') ) : the_row();
+
+						$title = get_sub_field('title');
+						$image = get_sub_field('image');
+						$link = get_sub_field('link');
+
+					?>
+
+
+						<li><a href="<?php echo $link; ?>" target="_blank" style="background-image: url('<?php echo $image['url']?>');">
+
+							<?php
+
+								if(empty($image)){
+									echo $title;
+								}
+								
+							?>
+							
+						</a></li>
+
+					<?php endwhile; ?>
+
+				</ul>
+				<!-- END list-banners -->
+
+				<?php
+
+				else :
+
+				// no rows found
+
+				endif;
+
+				?>
+
+				<div class="img-bear"></div>
+
+			</div>
+			<!-- END span6-->
 
 		</div>
 		<!-- END row -->
